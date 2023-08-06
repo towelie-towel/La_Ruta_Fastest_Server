03a4506000ed3ed8862c843135ecd96d0826c8e0
ackerman <julio.sergio2709@gmail.com>
2023-08-06 13:41:05 -0400

    just hello world

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,3 @@
 fn main() {
     println!("Hello, world!");
-    file
-        .lines()
-        .enumerate()
-        .filter(|(idx, _)| idx % 2 == 0)
-        .for_each(|( _, line)| println("{}", line));
 }
